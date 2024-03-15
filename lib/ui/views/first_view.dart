3cee4e6fa0aedcd4abf487a4c2b7094eb593b76d
santyagozaidan <santyagozaidan@gmail.com>
2024-03-15 19:26:35 +0000

    navigate to the register and login pages

--- lib/ui/views/first_view.dart
+++ lib/ui/views/first_view.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:myapp/ui/views/login_view.dart";
+import "package:myapp/ui/views/register_view.dart";
 
 class FirstView extends StatelessWidget {
   const FirstView({super.key});
@@ -9,32 +11,39 @@ class FirstView extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: size.width * 0.1,
-                vertical: size.height * 0.05
-              ),
-              child: const Text("Principal"),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.1, vertical: size.height * 0.05),
+            child: const Text("Principal"),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.2,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: size.width * 0.2,
-              ),
-              child: ElevatedButton(onPressed: ()=>{}, style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40)), child: const Text("Iniciar sesión")),
+            child: ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(builder: (builder) => const LoginView())),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(40)),
+                child: const Text("Iniciar sesión")),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.2,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: size.width * 0.2,
-              ),
-              child: ElevatedButton(onPressed: ()=>{}, style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40)), child: const Text("Registrarse")),
-            ),
-          ],
-        ),
-      ) 
-    );
+            child: ElevatedButton(
+                onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (builder) => const RegisterView())),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(40)),
+                child: const Text("Registrarse")),
+          ),
+        ],
+      ),
+    ));
   }
 }
