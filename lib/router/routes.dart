ca8fde60f6005e12db08223d2abf09663888fd96
santyagozaidan <santyagozaidan@gmail.com>
2024-03-15 19:16:36 +0000

    add routes

---
+++ lib/router/routes.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import "../ui/views/first_view.dart";
+import "../ui/views/login_view.dart";
+import "../ui/views/register_view.dart";
+
+var customRoutes = <String, WidgetBuilder>{
+  FirstView.id: (_) => const FirstView(),
+  LoginView.id: (_) => const LoginView(),
+  RegisterView.id: (_) => const RegisterView(),
+};
